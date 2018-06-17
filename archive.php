cecfbbabab10eac9978741c9a7e0d81f72251bac
Yoshie Takeda <info@dot-lab.xyz>
2018-06-18 06:41:33 +0900

    アーカイブページ追加
    カテゴリ一覧から飛んだ先

---
+++ archive.php
@@ -0,0 +1,64 @@
+<?php get_header(); ?>
+		<div id="main-area">
+			<?php if ( have_posts() && is_category() ) : ?>
+			<div id="page-info">
+				<h1><?php echo single_cat_title(); ?></h1>
+				<h2><?php echo strip_tags( category_description() ); ?></h2>
+			</div>
+			<div id="content">
+				<div id="side">
+					<h3 class="side-title">Category list</h3>
+					<ul>
+						<?php $categories = get_categories('parent=0'); ?>
+						<?php
+						foreach ( $categories as $val ) :
+						$cat_link = get_category_link($val->cat_ID);
+						?>
+						<li>
+							<a href="<?php echo $cat_link ?>">
+								<?php echo $val -> name ?>
+							</a>	
+						</li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+				<div id="blog-area">
+					<?php 
+					$cat  = get_the_category();
+					$cat  = $cat[0];
+					$cat  = $cat->cat_ID; 
+					$args = array (
+						'posts_per_page' => 10,
+						'post_type' => 'post',
+						'paged' => $paged,
+						'category' => $cat,
+					);
+					$posts_list = get_posts( $args );
+					?>
+					<?php foreach ( $posts_list as $post ) : setup_postdata( $post ); ?>
+					<div class="post">
+						<h4><?php the_title(); ?></h4>
+						<p class="post-meta">
+							<span class="post-date"><?php the_time('Y/m/d'); ?></span>
+							<span class="post-cat"><?php the_category(' '); ?></span>
+						</p>
+						<div class="post-content">
+							<?php the_content(); ?>
+						</div>
+					</div>
+					<?php endforeach; wp_reset_postdata(); ?>
+				</div>
+				<?php if ( function_exists('wp_pagenavi') ) : ?>
+				<div id="pager">
+					<?php wp_pagenavi(); ?>
+				</div>
+				<?php endif; ?>
+			</div>
+			<?php else : ?>
+			<div id="page-info">
+				<h1>Not found</h1>
+				<h2>お探しのページはみつかりませんでした</h2>
+			</div>
+			<?php endif; ?>
+		</div>
+<?php get_footer(); ?>
