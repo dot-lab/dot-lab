a044c96b53156d43bdf8c969697cded7889e8fe1
Yoshie Takeda <info@dot-lab.xyz>
2018-06-18 06:02:14 +0900

    blog固定ページのテンプレ
    ブログ記事一覧ページのテンプレを作成

--- template/blog.php
+++ template/blog.php
@@ -4,66 +4,62 @@
 */
 ?>
 <?php get_header(); ?>
-	<div id="container">
-		<div id="header">
-			<div id="navigation">
-				<div id="logo">
-					<a href="<?php echo home_url('/')?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png">
-					</a>
-				</div>
-				<div id="gnavi">
+		<div id="main-area">
+			<?php if ( have_posts() ) : ?>
+			<div class="page-info">
+				<h1><?php echo get_the_title(); ?></h1>
+				<h2><?php echo get_post_meta( $post->ID,'desc',true ); ?></h2>
+			</div>
+			<div id="content">
+				<div id="side">
+					<h3>Category list</h3>
 					<ul>
-						<li><a href="<?php echo home_url('/')?>works">Works</a></li>
-						<li><a href="<?php echo home_url('/')?>blog">Blog</a></li>
-						<li><a href="<?php echo home_url('/')?>contact">Contact</a></li>
+						<?php $categories = get_categories(); ?>
+						<?php
+						foreach ( $categories as $val ) :
+						$cat_link = get_category_link($val->cat_ID);
+						?>
+						<li>
+							<a href="<?php echo $cat_link; ?>">
+								<?php echo $val -> name; ?>
+							</a>	
+						</li>
+						<?php endforeach; ?>
 					</ul>
 				</div>
-			</div>
-			<div id="main-image">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/main-image.png">
-			</div>
-		</div>
-		<div id="main-col">
-			<div id="sidebar">
-				<ul>
-					<li>テストリスト</li>
-					<li>テストリスト</li>
-					<li>テストリスト</li>
-				</ul>
-			</div>
-			<div id="main">
-				<?php
-					$args = array(
+				<div id="blog-area">
+					<?php 
+					$args = array (
 						'posts_per_page' => 3,
 						'post_type' => 'post',
 						'paged' => $paged,
 					);
-					$postslist = get_posts($args);
-				?>
-				<?php if( have_posts($postslist) ) :?>
-					<?php foreach( $postslist as $post ) : setup_postdata($post); ?>
-						<div class="page-info">
-							<h1><?php the_title(); ?></h1>
-							<p class="desc">
-								<?php echo get_post_meta( $post->ID,'desc',true ); ?>
-							</p>
-						</div>
-						<div class="content">
+					$posts_list = get_posts( $args );
+					?>
+					<?php foreach ( $posts_list as $post ) : setup_postdata( $post ); ?>
+					<div class="post">
+						<h4><?php the_title(); ?></h4>
+						<p class="post-meta">
+							<span class="post-date"><?php the_time('Y/m/d'); ?></span>
+							<span class="post-cat"><?php the_category(' '); ?></span>
+						</p>
+						<div class="post-content">
 							<?php the_content(); ?>
 						</div>
-					<?php endforeach; ?>
-				<?php else :?>
-				<div class="page-info">
-					<h1>Not Found</h1>
-					<p class="desc">お探しのページはみつかりませんでした。</p>
+					</div>
+					<?php endforeach; wp_reset_postdata(); ?>
+				</div>
+				<?php if ( function_exists('wp_pagenavi') ) ?>
+				<div id="pager">
+					<?php wp_pagenavi(); ?>
 				</div>
 				<?php endif;?>
 			</div>
+			<?php else : ?>
+			<div id="page-info">
+				<h1>Not found</h1>
+				<h2>お探しのページはみつかりませんでした</h2>
+			</div>
+			<?php endif; ?>
 		</div>
-		<div class="clear"></div>
-		<div id="footer">
-			<p>.lab :: since 2016 :: by Yoshie Takeda</p>
-		</div>
-	</div>
 <?php get_footer(); ?>
