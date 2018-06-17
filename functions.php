f01086b911b7fb2417b340ff6fc6977b065eeb89
Yoshie Takeda <info@dot-lab.xyz>
2018-06-18 05:31:14 +0900

    基本部分の完成
    index.php と page.php を移植

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+	# 改行の自動挿入を無効化
 	remove_filter ( 'the_content', 'wpautop' );
 	remove_filter ( 'the_excerpt', 'wpautop' );
 ?>
--- index.php
+++ index.php
@@ -1,56 +1,20 @@
 <?php get_header(); ?>
-	<div id="container">
-		<div id="header">
-			<div id="navigation">
-				<div id="logo">
-					<a href="<?php echo home_url('/')?>">
-						<img src="<?php echo get_template_directory_uri(); ?>/images/logo.png">
-					</a>
+		<div id="main-area">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div id="page-info">
+					<h1><?php echo get_the_title(); ?></h1>
+					<h2><?php echo get_post_meta( $post->ID,'desc',true ); ?></h2>
 				</div>
-				<div id="gnavi">
-					<ul>
-						<li><a href="<?php echo home_url('/')?>works">Works</a></li>
-						<li><a href="<?php echo home_url('/')?>blog">Blog</a></li>
-						<li><a href="<?php echo home_url('/')?>contact">Contact</a></li>
-					</ul>
+				<div id="content">
+					<?php the_content(); ?>
 				</div>
-			</div>
-			<div id="main-image">
-				<img src="<?php echo get_template_directory_uri(); ?>/images/main-image.png">
-			</div>
-		</div>
-		<div id="main-col">
-			<div id="sidebar">
-				<ul>
-					<li>テストリスト</li>
-					<li>テストリスト</li>
-					<li>テストリスト</li>
-				</ul>
-			</div>
-			<div id="main">
-				<?php if( have_posts() ) :?>
-					<?php while( have_posts() ) : the_post(); ?>
-						<div class="page-info">
-							<h1><?php echo get_the_title(); ?></h1>
-							<p class="desc">
-								<?php echo get_post_meta( $post->ID,'desc',true ); ?>
-							</p>
-						</div>
-						<div class="content">
-							<?php the_content(); ?>
-						</div>
-					<?php endwhile; ?>
-				<?php else :?>
-				<div class="page-info">
-					<h1>Not Found</h1>
-					<p class="desc">お探しのページはみつかりませんでした。</p>
+			<?php endwhile; ?>
+			<?php else : ?>
+				<div id="page-info">
+					<h1>Not found</h1>
+					<h2>お探しのページはみつかりませんでした</h2>
 				</div>
-				<?php endif;?>
-			</div>
+			<?php endif; ?>
 		</div>
-		<div class="clear"></div>
-		<div id="footer">
-			<p>.lab :: since 2016 :: by Yoshie Takeda</p>
-		</div>
-	</div>
 <?php get_footer(); ?>
