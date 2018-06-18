a79ff7ccca37b0baa39ee066528b5a7d96ddf2f6
Yoshie Takeda <info@dot-lab.xyz>
2018-06-18 11:56:10 +0900

    single-product
    プロダクトのシングルページを作成,CSSの調整

---
+++ single-product.php
@@ -0,0 +1,50 @@
+<?php get_header();?>
+		<div id="main-area">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+			<?php $parent = get_post( 66 );
+			$parent_title = $parent->post_title;
+			?>
+				<div id="page-info">
+					<h1><?php echo $parent_title; ?></h1>
+					<h2><?php echo get_post_meta( $parent->ID,'desc',true ); ?></h2>
+				</div>
+				<div id="content">
+					<div id="product-single">
+						<div class="product-info">
+							<a href="<?php echo get_field('eyecatch'); ?>" class="swipebox" title="<?php echo get_field('title');?>">
+								<img src="<?php echo get_field('eyecatch'); ?>" class="eyecatch">
+							</a>
+							<?php
+							$execute = get_field('execute'); 
+							if ( $execute ) :
+							?>
+							<p class="execute-link">
+								<a href="<?php echo $execute; ?>" target="_blank">実行可能ファイルなど（外部リンク）</a>
+							</p>
+							<?php endif;?>
+							<?php
+							$github = get_field('github');
+							if ( $github ) :
+							?>
+							<p class="github-link">
+								<a href="<?php get_field('github'); ?>" target="_blank">ソースコード（GitHub）</a>
+							</p>
+							<?php endif;?>
+						</div>
+						<div class="description">
+							<h3 class="product-title"><?php echo get_field('title');?></h3>
+							<?php echo get_field('description');?>
+						</div>
+						<div class="clear"></div>
+					</div>
+				</div>
+				<?php endwhile; ?>
+				<?php else : ?>
+					<div id="page-info">
+						<h1>Not found</h1>
+						<h2>お探しのページはみつかりませんでした</h2>
+					</div>
+				<?php endif; ?>
+			</div>
+<?php get_footer();?>
--- template/product.php
+++ template/product.php
@@ -4,52 +4,37 @@
 */
 ?>
 <?php get_header();?>
-			<div class="main">
-				<?php if ( have_posts() ) : ?>
-					<div class="center">
-						<h1><?php echo get_the_title(); ?></h1>
-						<p class="desc">
-							<?php echo get_post_meta( $post->ID,'desc',true ); ?>
-						</p>
-					</div>
-					<?php /* ?>
-					<div class="side">
-						<h3>Category List</h3>
-						<?php $categories = get_categories('parent=0');
-						foreach ( $categories as $val ) : 
-							$cat_link = get_category_link($val->cat_ID);?>
-							<ul>
-								<li>
-									<a href="<?php echo $cat_link?>"><?php echo $val->name ?></a>
-								</li>
-							</ul>
-						<?php endforeach; ?>
-					</div>
-					<?php */ ?>
-					<div class="posts">
+		<div id="main-area">
+		<?php if ( have_posts() ) : ?>
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div id="page-info">
+					<h1><?php echo get_the_title(); ?></h1>
+					<h2><?php echo get_post_meta( $post->ID,'desc',true ); ?></h2>
+				</div>
+				<div id="content">
 					<?php
-					$args = array(
+					$args = array (
 						'posts_per_page' => -1,
 						'post_type' => 'product',
 					);
-					$postslist = get_posts( $args ); ?>
-					<?php foreach ( $postslist as $post ) :setup_postdata( $post );?>
-							<div class="product">
-								<a href="<?php echo get_permalink();?>">
-									<div class="eyecatch">
-										<img src="<?php echo get_field('eyecatch'); ?>">
-									</div>
-									<p class="product_title">
-										<?php echo get_field('title');?>
-									</p>
-								</a>
-							</div>
+					$posts = get_posts( $args );
+					?>
+					<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+						<div class="product">
+							<a href="<?php echo get_permalink(); ?>" class="bdr-none">
+								<img src="<?php echo get_field('eyecatch'); ?>" class="eyecatch">
+								<span class="product-title"><?php echo get_field('title');?></span>
+							</a>
+							<span class="product-cat"><?php echo get_field('category');?></span>
+						</div>
 					<?php endforeach; wp_reset_postdata(); ?>
 				</div>
+				<?php endwhile; ?>
 				<?php else : ?>
-					<h1>Not Found</h1>
-					<p style="text-align: center;">お探しのページは見つかりませんでした。</p>
+					<div id="page-info">
+						<h1>Not found</h1>
+						<h2>お探しのページはみつかりませんでした</h2>
+					</div>
 				<?php endif; ?>
-				<div class="clear"></div>
 			</div>
 <?php get_footer();?>
